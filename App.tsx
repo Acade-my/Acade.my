146ef305384a65dfde2c61eee64aa03ecbcab4a0
Acade-my <hsjssu117@gmail.com>
2025-04-06 21:24:56 +0900

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="p-8 text-center">
+      <h1 className="text-4xl font-bold">정 리</h1>
+      <p className="text-lg mt-2 text-neutral-500">나의 것으로 만들기 위한 정리들</p>
+    </div>
+  );
+}
